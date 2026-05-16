529782b1d31aaede0712358f9a2cc6b50835fea7
Chris Gordon <chris.c.gordon888@gmail.com>
2026-05-16 12:05:06 -0500

    Improve mobile realm soundstage layout

--- frontend/src/app/realms/303/page.tsx
+++ frontend/src/app/realms/303/page.tsx
@@ -303,6 +303,7 @@ export default function Realm303() {
                             progress={realmProgress}
                             isUnlocked={true}
                             isCurrentRealm={true}
+                            compactOnMobile
                         />
                     </div>
 
